a147e7e895d43934c16acad76fb9ed1e6a24a451
Harshil Shah <78573099+shah-harshil-07@users.noreply.github.com>
2024-02-23 18:59:06 +0530

    Delete graphql-demo directory

--- graphql-demo/src/graphql/index.js
+++
@@ -1,9 +0,0 @@
-import { GET_POSTS } from "./queries/posts"
-import { CREATE_POST, UPDATE_POST, DELETE_POST } from "./mutations/posts"
-
-export const PostGQLQueries = {
-    GET_POSTS,
-    CREATE_POST,
-    UPDATE_POST,
-    DELETE_POST
-};
